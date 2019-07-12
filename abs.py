377b3f6f3aacddb3ee548ba33fd79b7c6586f431
IndhuVelu <52819973+IndhuVelu@users.noreply.github.com>
2019-07-12 15:44:16 +0530

    Update abs.py

--- abs.py
+++ abs.py
@@ -1,6 +1,6 @@
 n=input()
 if(n=='a' or n=='e' or n=='i' or n=='o' or n=='u' or n=='A' or n=='E' or n=='I' or n=='O' or n=='U'):
-  print("Vowels")
+  print("Vowel")
 elif(n=='!' or n=='@' or n=='#' or n=='$' or n=='^' or n=='&' or n=='*' or n=='(' or n==')' or n=='{' or n=='}' or n=='[' or n==')' or n=='/' or n=='-'):
     print("Invalid")
 else:
