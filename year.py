3a6b833985b2bc860d6abd4b699c27639eed282d
IndhuVelu <52819973+IndhuVelu@users.noreply.github.com>
2019-07-12 16:46:37 +0530

    Update year.py

--- year.py
+++ year.py
@@ -1,6 +1,11 @@
 y=int(input())
-if y%400==0 and y%100!=0:
-  if y%4==0:
-    print("Yes")
+if y%4==0:
+  if y%100==0:
+    if y%400==0:
+      print("Yes")
+    else:
+      print("No")
   else:
-    print("No")
+    print("Yes")
+else:
+  print("No")
