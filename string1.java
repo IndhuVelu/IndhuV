7749855a951fa8c4edfc9d3a89639968c978eb1f
IndhuVelu <52819973+IndhuVelu@users.noreply.github.com>
2019-07-13 17:32:41 +0530

    Update string1.java

--- string1.java
+++ string1.java
@@ -1,42 +1,26 @@
-import java.util.*;
-class one
-{
-	static String areisomorphic(String str1,String str2)
-	{
+MAX_CHARS = 100002
+def areIsomorphic(string1, string2):
+    m = len(string1)
+    n = len(string2)
+    if m != n:
+        return False
+    marked = [False] * MAX_CHARS
+    map = [-1] * MAX_CHARS
+    for i in range(n):
+        if map[ord(string1[i])] == -1:
+            if marked[ord(string2[i])] == True:
+                return False
+            marked[ord(string2[i])] = True
+            map[ord(string1[i])] = string2[i]
+        elif map[ord(string1[i])] != string2[i]:
+            return False
 
-		int len_str1=str1.length();
-		int len_str2=str2.length();
-		int map[]=new int[256];
-		Arrays.fill(map,-1);
-		boolean marked[]=new boolean[256];
-		Arrays.fill(marked,false);
-		if(len_str1!=len_str2)
-		{
-			return "no";
-		}
-		else
-		{
-			for(int i=0;i<len_str1;i++)
-			{
-				if(map[str1.charAt(i)]==-1)
-				{
-					if(marked[str2.charAt(i)]==true)
-						return "no";
-					marked[str2.charAt(i)]=true;
-					map[str1.charAt(i)]=str2.charAt(i);
+    return True
 
-				}
-				else if(map[str1.charAt(i)]!=str2.charAt(i))
-					return "no";
-			}
-			return "yes";
-		}
-	}
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		String str1=sc.next();
-		String str2=sc.next();
-		System.out.println(areisomorphic(str1,str2));
-	}
-} 
+word1=input()
+word2=input()
+if areIsomorphic(word1,word2):
+    print("yes")
+else:
+    print("no")
+
