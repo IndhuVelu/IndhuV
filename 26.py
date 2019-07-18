c337da9cb8ecb948088d27a5f2f1c1ef7b5940a6
IndhuVelu <52819973+IndhuVelu@users.noreply.github.com>
2019-07-18 23:32:20 +0530

    Update 26.py

--- 26.py
+++ 26.py
@@ -2,12 +2,12 @@ n1=int(input())
 l1=list(map(int,input().split()))
 choc=1
 c1=0
-if l1[0]>1[1]:
+if l1[0]>11[1]:
     choc=choc+1
     c1=c1+choc
 else:
     c1=choc
-for i in range(1,len(lis)):
+for i in range(11,len(lis)):
     if l1[i]>l1[i-1]:
         choc=choc+1
         c1=c1+choc
