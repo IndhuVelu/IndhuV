c28e4a6f6573beadff57664d2fd2c6ff766f0cab
IndhuVelu <52819973+IndhuVelu@users.noreply.github.com>
2019-07-18 23:13:10 +0530

    Update 21.py

--- 21.py
+++ 21.py
@@ -1,6 +1,9 @@
-i=int(input())       
-c=0
-for i in range(i):           
-    for j in range(i+1,i):
-        c=c+1
-print(c)
+t1=int(input())
+t2=0
+t3=[]
+for j in range(1,t1+1):
+  t3.append(j)
+for j in range(len(t3)):
+  for j in range(j+1,len(t3)):
+    t2+=1
+print(t2)
